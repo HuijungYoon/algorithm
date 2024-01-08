9b19a1dfabf19dcd00589bfb36fcc3a83209faf3
HuijungYoon <markup3604@gmail.com>
2024-01-08 20:40:05 +0900

    1012 배추밭 Dfs문제

--- 0108/1012.cpp
+++ 0108/1012.cpp
@@ -1,59 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dy[4] ={-1,0,1,0};
-int dx[4] ={0,1,0,-1};
-bool visited[51][51];
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
 int a[51][51];
-int n,m,k,x,y,ret,ny,nx,t;
+bool visited[51][51];
+int n,m,k,x,y,ny,nx,t,ret;
 
-
-void dfs(int y, int x)
+void dfs(int y,int x)
 {
     visited[y][x] = 1;
-
     for(int i=0; i<4; i++)
     {
-     ny = y + dy[i];
-     nx = x + dx[i];
-    if(ny<0 || ny>=n || nx<0 || nx<=m) continue;
-    if(a[ny][nx] = 1 && !visited[ny][nx])
-    {
-        dfs(ny,nx);
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny<0 || nx <0 || ny>=n || nx>=m ) continue;
+        if(!visited[ny][nx] && a[ny][nx] == 1)
+        {
+            dfs(ny,nx);
+        }
+
     }
-    }
-    return ;
+    return;
 }
 
 
-int main()  {
+
+
+int main() {
+
     cin >> t;
 
     while(t--)
     {
         fill(&a[0][0],&a[0][0] + 51 * 51,0);
-       fill(&visited[0][0],&visited[0][0] + 51 * 51,0);
-       ret = 0;
-       cin >> m >> n >> k;
-       for(int i=0; i<k; i++)
-       {
-        cin >> x >> y;
-        a[x][y] = 1;
-       }
-
-       for(int i = 0; i<n; i++)
-       {
-        for(int j=0; j<m; j++)
+        fill(&visited[0][0],&visited[0][0] + 51 * 51,0);
+        ret = 0;
+        cin >> m >> n >> k;
+        for(int i=0; i<k; i++)
         {
-            if(a[i][j] == 1 && !visited[i][j])
-            {
-                dfs(i,j);
-                ret ++;
+            cin >> x >> y;
+            a[y][x] = 1;
+        }
 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(a[i][j] == 1 && !visited[i][j])
+                {
+                    dfs(i,j);
+                    ret++;
+                }
             }
         }
-       }
-       cout << ret << "\n";
+
+        cout << ret << "\n";
     }
+
+
+
 
     return 0;
 }
