8e4f6fdf4d7656b2dc5de1df6d179bb8d42eb2c3
HuijungYoon <markup3604@gmail.com>
2024-01-22 20:16:31 +0900

    10709 기상캐스터 answer

--- 0118/10709.cpp
+++ 0118/10709.cpp
@@ -17,16 +17,32 @@ int main() {
     }
 
     for(int i=0; i<h; i++){
+        flag = 0;
         for(int j=0; j<w; j++){
             if(a[i][j] == "c"){
                 cnt = 0;
                 flag = 1;
+                ret[i][j] = cnt;
+
             }
+            if(flag && a[i][j] == "."){
+                cnt++;
+                ret[i][j] = cnt;
 
+            } else if(!flag) {
+                flag = 0;
+                ret[i][j] = -1;
+
+            }
         }
-    }
+    };
 
-//continue ....
+    for(int i=0; i<h; i++){
+        for(int j=0; j<w; j++){
+            cout << ret[i][j] << " ";
+        }
+        cout << "\n";
+    }
 
 
 return 0;
