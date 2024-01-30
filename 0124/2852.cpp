5fee819ac6c520bc50d8f0644f3d6984b95405cd
HuijungYoon <markup3604@gmail.com>
2024-01-30 20:40:08 +0900

    1436 숌

--- 0124/2852.cpp
+++ 0124/2852.cpp
@@ -2,37 +2,39 @@
 using namespace std;
 #define prev huijung
 int n,o,A,B,asum,bsum;
-string s, prev;
+string prev,s;
 
-string print(int a){
-    string d = "00" + to_string(a / 60);
-    string e = "00" + to_string(a % 60);
-    return d.substr(d.size() -2 ,2) + ":" + e.substr(e.size() -2,2 );
+string print(int a) {
+    string d = "00" + to_string(a/60);
+    string e = "00" + to_string(a%60);
+
+    return d.substr(d.size()-2,2) + ":" + e.substr(e.size()-2,2);
 }
 
-int changeToInt(string a){
+
+int changeToInt(string a) {
+
     return atoi(a.substr(0,2).c_str()) * 60 + atoi(a.substr(3,2).c_str());
 }
 
-void go(int &sum, string s){
-    sum += (changeToInt(s) - changeToInt(prev));
+void go(int &sum, string s) {
+     sum += changeToInt(s) - changeToInt(prev);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    for(int i=0; i<n; i++) {
+    for(int i=0; i<n; i++){
         cin >> o >> s;
-        if(A > B) go(asum,s);
+        if(A>B) go(asum,s);
         else if(A<B) go(bsum,s);
-        o == 1 ? A++ : B++;
-        prev = s;
+        o == 1 ? A++ : B++; 
+        prev =s;
     }
-    if(A > B) go(asum, "48:00");
-    else if(B>A) go(bsum, "48:00");
+    if(A > B)go(asum, "48:00");
+    else if(B > A)go(bsum, "48:00"); 
     cout << print(asum) << "\n";
     cout << print(bsum) << "\n";
+
 
     return 0;
 }
