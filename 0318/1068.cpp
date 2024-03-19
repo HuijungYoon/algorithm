f4a9f080003ccde2130ca19584a95a18d2e2d359
HuijungYoon <markup3604@gmail.com>
2024-03-19 21:03:29 +0900

    1325 dfs

--- 0318/1068.cpp
+++ 0318/1068.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,r, temp, root;
+int n,temp,r,root;
 vector<int> adj[54];
 
-
 int dfs(int here){
-    int ret = 0;
     int child = 0;
+    int ret = 0;
     for(int there : adj[here]){
         if(there == r) continue;
-        ret += dfs(there);
+        ret += dfs(there)
         child++;
     }
     if(child == 0) return 1;
@@ -18,16 +17,17 @@ int dfs(int here){
 
 int main() {
     cin >> n;
-
     for(int i=0; i<n; i++){
         cin >> temp;
         if(temp == -1) root = i;
         else adj[temp].push_back(i);
     }
     cin >> r;
-    if(r == root){
+    if(root == r) {
         cout << 0 << "\n"; return 0;
     }
-    cout << dfs(root) << "\n";
-    return  0;
+    cout <<dfs(root) << "\n";
+
+
+    return 0;
 }
